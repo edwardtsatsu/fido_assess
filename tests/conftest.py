b66e1e1d5a61536338825cf3992bf1d313bec6e6
edwardtsatsu <edwardakorlie73@gmail.com>
2024-10-06 23:39:36 +0000

    black

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-# tests/conftest.py
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,21 +5,17 @@ from sqlalchemy.orm import sessionmaker
 from configs.database import Base
 from src.models.user import User
 
-# Test database URL for SQLite
+
 TEST_DATABASE_URL = "sqlite:///./test.db"
 
-# Create a new SQLAlchemy engine
 engine = create_engine(TEST_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @pytest.fixture(scope="session")
 def db_session():
-    """Create a new database session for a test."""
-    # Create the database tables for testing
     Base.metadata.create_all(bind=engine)
 
-    # Create a new session for the test
     db = SessionLocal()
     yield db
 
@@ -31,7 +26,6 @@ def db_session():
 
 @pytest.fixture(scope="function", autouse=True)
 def clean_db(db_session):
-    """Clean the database after each test."""
     yield
     db_session.query(User).delete()
     db_session.commit()
--- tests/unit/seeders/test_user_seeder.py
+++ tests/unit/seeders/test_user_seeder.py
@@ -1,33 +1,24 @@
-# tests/unit/seeder/test_user_seeder.py
-import pytest
-
 from seeders.user_seeder import UserSeeder
 from src.models import User
 
 
 def test_user_seeder(db_session):
-    """Test the UserSeeder to ensure it adds a user to the test database."""
-    # Ensure no user exists before running the seeder
     initial_user_count = db_session.query(User).count()
     assert initial_user_count == 0, "No users should exist before seeding."
 
     seeder = UserSeeder(db_session=db_session)
 
-    # Run the seeder
     seeder.run()
 
-    # Check if the user was added
     user = db_session.query(User).filter(User.email == "test@gmail.com").first()
 
-    # Assertions
-    assert user is not None, "User should have been added to the database"
-    assert user.first_name == "test", "User first name should match"
-    assert user.last_name == "test", "User last name should match"
-    assert user.email == "test@gmail.com", "User email should match"
-    assert user.password != "test", "User password should be hashed"
-    assert user.password is not None, "User password should not be None"
+    assert user is not None
+    assert user.first_name == "test"
+    assert user.last_name == "test"
+    assert user.email == "test@gmail.com"
+    assert user.password != "test"
+    assert user.password is not None
 
-    # Print the user's ID and name
     print(f"User ID: {user.id}, User Name: {user.first_name} {user.last_name}")
 
     final_user_count = db_session.query(User).count()
