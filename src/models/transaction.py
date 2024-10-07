205013c49165ca311bfab65319fc53f9d21f54ed
edwardtsatsu <edwardakorlie73@gmail.com>
2024-10-07 10:30:33 +0000

    updated

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -1,8 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime
+from sqlalchemy import (
+    Boolean,
+    CheckConstraint,
+    Column,
+    DateTime,
+    Index,
+    UniqueConstraint,
+)
 from sqlalchemy import Enum as SQLALchemyEnum
-from sqlalchemy import ForeignKey, Integer, Numeric, String, func
+from sqlalchemy import ForeignKey, Integer, String, func
 from sqlalchemy.orm import relationship
 
 from src.constants.transaction_type import TransactionType
@@ -13,9 +20,11 @@ class Transaction(Base):
     __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     date = Column(DateTime)
-    exttrid = Column(String, nullable=False)
+    exttrid = Column(String, nullable=False, unique=True)
     amount = Column(Integer, nullable=False)
     type = Column(SQLALchemyEnum(TransactionType), nullable=False)
     description = Column(String, nullable=False)
@@ -24,3 +33,11 @@ class Transaction(Base):
     updated_at = Column(DateTime, server_onupdate=func.now(), nullable=True)
 
     user = relationship("User", back_populates="transactions")
+
+    __table_args__ = (
+        UniqueConstraint("exttrid", name="uq_exttrid"),
+        CheckConstraint("amount > 0", name="check_amount_positive"),
+        Index("idx_user_id", "user_id"),
+        Index("idx_exttrid", "exttrid"),
+        Index("idx_date", "date"),
+    )
